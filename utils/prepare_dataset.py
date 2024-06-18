29f59cf2f4c650dcfe1212f978d0884cc7bc62b7
Etienne <etienne.bardet@gmail.com>
2024-06-18 05:37:56 +0000

    find cloudless images

--- utils/prepare_dataset.py
+++ utils/prepare_dataset.py
@@ -1,6 +1,8 @@
 import os
+import numpy as np
 
 clouds_path = os.path.join('/home/shared/Data/cloud_masks')
+sen3_path = os.path.join('/home/shared/Data/OLCI/GoM/')
 
 def filter_masks_by_percentage(low_bound=0, high_bound=25):
     """
@@ -26,6 +28,27 @@ def filter_masks_by_percentage(low_bound=0, high_bound=25):
     #Both operations could be condensed into one line but for readability they are separated
 
     print("There are {} cloud masks between {} and {} percent of coverage".format(len(files), low_bound, high_bound))
+    return files
+
+def fetch_sen3(path):
+    """
+        This function fetches the sen3 files from the directory.
+        It looks into all the .SEN3 folders and returns the paths to the ones where the matrix of cloud batches is empty (no clouds).
+
+        Args:
+        path: the path to the directory containing the .SEN3 folders
+
+        Returns:
+        files: a list of paths to the .SEN3 folders where the matrix of cloud batches is empty
+
+    """
+    files = []
+    for dirs in os.listdir(path):
+        for name in os.listdir(os.path.join(path,dirs)):
+            if name.endswith('.SEN3'):
+                if(np.load(os.path.join(path,dirs, name, 'cloud_batch_coords_256.npy')).size == 0):
+                    files.append(os.path.join(path, name))
+    print("There are {} .SEN3 folders with no clouds".format(len(files)))
     return files
 
 
