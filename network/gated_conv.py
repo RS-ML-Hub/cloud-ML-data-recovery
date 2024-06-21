fbbc72a2f7ebdff5dfd3a0c8691465336a2e1b2e
Etienne <etienne.bardet@gmail.com>
2024-06-21 07:39:39 +0000

    tweaks on the model

--- network/gated_conv.py
+++ network/gated_conv.py
@@ -1,20 +1,19 @@
-
 import tensorflow as tf
 import keras
 
-#Implementation of the GatedConv2D and GatedDeConv2D classes as per both 
-#https://github.com/Oliiveralien/Inpainting-on-RSI/ and https://github.com/JiahuiYu/generative_inpainting/
+# Implementation of the GatedConv2D and GatedDeConv2D classes as per both 
+# https://github.com/Oliiveralien/Inpainting-on-RSI/ and https://github.com/JiahuiYu/generative_inpainting/
 
 class GatedConv2D(keras.Model):
-    def __init__(self, cn_out, ker_size=5, stride=1, dilation=1, activation='relu'):
-        super(GatedConv2D,self).__init__()
-        #TODO determine if bn is needed 
-        self.ac=activation
+    def __init__(self, cn_out, ker_size=5, stride=1, dilation=1, activation='relu', trainable=True, dtype=tf.float32):
+        super(GatedConv2D, self).__init__()
+        # TODO determine if bn is needed 
+        self.ac = activation
         self.bn = keras.layers.BatchNormalization()
-        self.act = keras.layers.Activation(activation)
-        self.conv = keras.layers.Conv2D(filters=cn_out,strides=stride, kernel_size=ker_size, padding='same', activation=None, dilation_rate=dilation)
-        self.gate = keras.layers.Conv2D(filters=cn_out,strides=stride, kernel_size=ker_size, padding='same', activation=None, dilation_rate=dilation)
-        self.sigmoid = keras.layers.Activation('sigmoid')
+        self.act = keras.layers.ReLU()
+        self.conv = keras.layers.Conv2D(filters=cn_out, strides=stride, kernel_size=ker_size, padding='same', activation=None, dilation_rate=dilation, kernel_initializer='he_normal')
+        self.gate = keras.layers.Conv2D(filters=cn_out, strides=stride, kernel_size=ker_size, padding='same', activation=None, dilation_rate=dilation, kernel_initializer='he_normal')
+        self.sigmoid = keras.layers.Activation('sigmoid')   
     def call(self, input):
         x = self.conv(input)
         gated = self.gate(input)
@@ -22,15 +21,21 @@ class GatedConv2D(keras.Model):
             return self.bn(x * self.sigmoid(gated))
         else:
             return self.bn(self.act(x) * self.sigmoid(gated))
-        
+    
 
 
 class GatedDeConv2D(keras.Model):
-    def __init__(self, scale, cn_out, ker_size, stride, dilation=1):
-        super(GatedDeConv2D,self).__init__()
-        self.upsample =  keras.layers.UpSampling2D(size=(scale,scale), interpolation='nearest')
-        self.conv = GatedConv2D(cn_out, ker_size, stride, dilation, activation=keras.layers.LeakyReLU(0.2))
-
+    def __init__(self, scale, cn_out, ker_size, stride, dilation=1, trainable=True, dtype=tf.float32):
+        super(GatedDeConv2D, self).__init__()
+        self.upsample = keras.layers.UpSampling2D(size=(scale, scale), interpolation='nearest')
+        self.bn = keras.layers.BatchNormalization()
+        self.act = keras.layers.LeakyReLU(negative_slope=0.2)
+        self.conv = keras.layers.Conv2D(filters=cn_out, strides=stride, kernel_size=ker_size, padding='same', activation=None, dilation_rate=dilation, kernel_initializer='he_normal')
+        self.gate = keras.layers.Conv2D(filters=cn_out, strides=stride, kernel_size=ker_size, padding='same', activation=None, dilation_rate=dilation, kernel_initializer='he_normal')
+        self.sigmoid = keras.layers.Activation('sigmoid')   
+    
     def call(self, input):
         x = self.upsample(input)
-        return self.conv(x)
+        mask = self.gate(x)
+        x = self.conv(x)
+        return self.bn(self.act(x) * self.sigmoid(mask))
--- network/loss.py
+++ network/loss.py
@@ -1,4 +1,10 @@
 import tensorflow as tf
 
 def lossL1(y_true, y_pred):
-    return tf.reduce_mean(tf.abs(y_true - y_pred))
+    for chan in range(11):
+        chan_loss = tf.reduce_mean(tf.abs(y_true[:,:,:,chan] - y_pred[:,:,:,chan]))
+        if chan == 0:
+            loss = chan_loss
+        else:
+            loss = loss + chan_loss
+    return loss
