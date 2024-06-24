19e696072b0852925ee35ae31a2584a8ddc950c2
Etienne <etienne.bardet@gmail.com>
2024-06-24 07:16:50 +0000

    modify loss

--- network/loss.py
+++ network/loss.py
@@ -1,10 +1,9 @@
 import tensorflow as tf
 
-def lossL1(y_true, y_pred):
-    for chan in range(11):
-        chan_loss = tf.reduce_mean(tf.abs(y_true[:,:,:,chan] - y_pred[:,:,:,chan]))
-        if chan == 0:
-            loss = chan_loss
-        else:
-            loss = loss + chan_loss
-    return loss
+def lossL1(y_true, y_pred, mask):
+    y_pred = y_pred[:,:,:,0]
+    chan1 = tf.reduce_mean(tf.abs(y_true[:,:,:] - y_pred[:,:,:])*(mask[:,:,:])/tf.reduce_mean(mask[:,:,:], keepdims=True))
+    chan2 = tf.reduce_mean(tf.abs(y_true[:,:,:] - y_pred[:,:,:])*(1- mask[:,:,:])/(1-tf.reduce_mean(mask[:,:,:], keepdims=True)))
+    chan_loss = chan1 + chan2
+
+    return chan_loss
