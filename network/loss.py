4ae43bf698c0fbde352958a711524e2c2e0fd145
Etienne <etienne.bardet@gmail.com>
2024-07-04 07:09:15 +0000

    fix all losses

--- network/loss.py
+++ network/loss.py
@@ -12,24 +12,31 @@ def GeneratorLoss(neg):
 def coarseLoss(y_true, y_pred, mask):
     mask = mask[:,:,:,0]
     chan_loss = 0
+    mask_vectorized= tf.reshape(mask, [tf.shape(mask)[0],-1])
+    mvm = tf.reduce_mean(mask_vectorized, axis=1)
+    mvmr = tf.reshape(mvm, [-1,1,1,1])
     for i in range(11):
-      chan1 = tf.reduce_mean(tf.abs(y_true[:,:,:,i] - y_pred[:,:,:,i])*(mask[:,:,:])/tf.reduce_mean(tf.reshape(mask[:,:,:], (-1,1,1))))
-      chan2 = tf.reduce_mean(tf.abs(y_true[:,:,:,i] - y_pred[:,:,:,i])*(1- mask[:,:,:])/(1-tf.reduce_mean(tf.reshape(mask[:,:,:], (-1,1,1)))))
+      chan1 = tf.reduce_mean(tf.abs(y_true[:,:,:,i] - y_pred[:,:,:,i])*(mask[:,:,:])/(mvmr))
+      chan2 = tf.reduce_mean(tf.abs(y_true[:,:,:,i] - y_pred[:,:,:,i])*(1- mask[:,:,:])/(1-mvmr))
       chan_loss += chan1 + chan2
     return chan_loss
 
 def reconstructLoss(y_true, y_pred,mask):
     mask = mask[:,:,:,0]
     chan_loss = 0
+    mask_vectorized= tf.reshape(mask, [tf.shape(mask)[0],-1])
+    mvm = tf.reduce_mean(mask_vectorized, axis=1)
+    mvmr = tf.reshape(mvm, [-1,1,1,1])
     for i in range(11):
-      chan1 = tf.reduce_mean(tf.abs(y_true[:,:,:,i] - y_pred[:,:,:,i])*(mask[:,:,:])/tf.reduce_mean(tf.reshape(mask[:,:,:], (-1,1,1))))
-      chan2 = tf.reduce_mean(tf.abs(y_true[:,:,:,i] - y_pred[:,:,:,i])*(1- mask[:,:,:])/(1-tf.reduce_mean(tf.reshape(mask[:,:,:], (-1,1,1)))))
+      chan1 = tf.reduce_mean(tf.abs(y_true[:,:,:,i] - y_pred[:,:,:,i])*(mask[:,:,:])/(mvmr))
+      chan2 = tf.reduce_mean(tf.abs(y_true[:,:,:,i] - y_pred[:,:,:,i])*(1- mask[:,:,:])/(1-mvmr))
       chan_loss += chan1 + chan2
     return chan_loss
 
 
-def gram_matrix(input_tensor):
-    return tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor) / tf.cast(input_tensor.shape[1]*input_tensor.shape[2]*input_tensor.shape[3], tf.float32)
+def gram_matrix(x):
+    gram = tf.reshape(x, (tf.shape(x)[0], tf.shape(x)[1], tf.shape(x)[2]*tf.shape(x)[3]))
+    return tf.matmul(gram, tf.transpose(gram, perm=[0,2,1]))
 
 def StyleLoss(feats_img, feats_refined_img):
     #Calculate style loss
@@ -37,7 +44,7 @@ def StyleLoss(feats_img, feats_refined_img):
     for i in range(len(feats_img)):
         gram_img = gram_matrix(feats_img[i])
         gram_refined_img = gram_matrix(feats_refined_img[i])
-        style_loss += tf.reduce_mean(tf.abs(gram_img - gram_refined_img))
+        style_loss += (tf.reduce_mean(tf.abs(gram_img - gram_refined_img)))/tf.cast(tf.shape(feats_img[i])[2]*tf.shape(feats_img[i])[3], tf.float32)
     return style_loss
 
 def PerceptualLoss(feats_img, feats_refined_img):
